3675162c2dc16b091c74971822db3338b775fc43
syz <a827871781@gmail.com>
2019-11-28 17:24:34 +0800

    密码修改

--- src/main/resources/db/migration/V2__Initial.sql
+++ src/main/resources/db/migration/V2__Initial.sql
@@ -1,1 +1,6 @@
-insert into test (name,test2) values ("123","21321")
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `test2` varchar(25) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
